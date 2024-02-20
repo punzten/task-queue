1befb6a376925b0ded3ac01148c3060e14eb3d40
punzten <123665742+punzten@users.noreply.github.com>
2024-02-21 01:35:14 +0400

    Init

---
+++ task_queue.py
@@ -0,0 +1,66 @@
+"""
+Task description:
+* Requires a task queue with priorities and resource limits.
+* Each task has a priority and the required amount of resources to process it.
+* Publishers create tasks with specified resource limits, and put them in a task queue.
+* Consumer receives the highest priority task that satisfies available resources.
+* The queue is expected to contain thousands of tasks.
+* Write a unit test to demonstrate the operation of the queue.
+"""
+
+from dataclasses import dataclass, fields
+from queue import PriorityQueue
+from functools import total_ordering
+
+
+@dataclass()
+class Resources:
+    ram: int
+    cpu_cores: int
+    gpu_count: int
+
+
+@total_ordering
+@dataclass(eq=False)
+class Task:
+    id: int
+    priority: int
+    resources: Resources
+    content: str
+    result: str = ""
+
+    def __eq__(self, other):
+        return self.priority == other.priority
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+
+class TaskQueue:
+    def __init__(self):
+        self.queue = PriorityQueue()
+
+    def size(self) -> int:
+        return self.queue._qsize()
+
+    def add_task(self, task: Task):
+        self.queue.put(task)
+
+    def get_task(self, available_resources: Resources) -> Task:
+        not_satisfied = []
+
+        while not self.queue.empty():
+            task = self.queue.get()
+
+            if all(
+                    [getattr(available_resources, field.name) >= getattr(task.resources, field.name)
+                     for field in fields(Resources)]
+            ):
+                return task
+            else:
+                not_satisfied.append(task)
+
+        for task in not_satisfied:
+            self.queue.put(task)
+
+        return None
---
+++ unit_tests.py
@@ -0,0 +1,56 @@
+import pytest
+from task_queue import Resources, Task, TaskQueue
+
+
+@pytest.fixture
+def task_queue():
+    return TaskQueue()
+
+
+@pytest.fixture
+def resources0():
+    return Resources(ram=0, cpu_cores=1, gpu_count=0)
+
+
+@pytest.fixture
+def resources():
+    return Resources(ram=512, cpu_cores=1, gpu_count=0)
+
+
+@pytest.fixture
+def resources2():
+    return Resources(ram=1024, cpu_cores=2, gpu_count=0)
+
+
+@pytest.fixture
+def task(resources):
+    return Task(id=1, priority=100, resources=resources, content='content')
+
+
+@pytest.fixture
+def task2(resources):
+    return Task(id=2, priority=10, resources=resources, content='content')
+
+
+def test_simple_add(task_queue, task):
+    task_queue.add_task(task)
+    assert task_queue.size() == 1
+
+
+def test_simple_pop(task_queue, resources, task):
+    task_queue.add_task(task)
+    assert task_queue.get_task(available_resources=resources) == task
+    assert task_queue.size() == 0
+
+
+def test_resources_constraint(task_queue, task, resources0):
+    task_queue.add_task(task)
+    task_queue.get_task(available_resources=resources0)
+    assert task_queue.size() == 1
+
+
+def test_priority_order(task_queue, task, task2, resources2):
+    task_queue.add_task(task)
+    task_queue.add_task(task2)
+    t = task_queue.get_task(available_resources=resources2)
+    assert t.priority == min(task.priority, task2.priority)
